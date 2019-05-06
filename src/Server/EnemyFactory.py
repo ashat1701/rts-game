86f3b55c808dbd3b868838f00343daa59860a4b6
ashat1701 <ashat1701@github.com>
2019-05-06 19:35:46 +0300

    add enemy factory

---
+++ src/Server/EnemyFactory.py
@@ -0,0 +1,14 @@
+class EnemyFactory:
+    def __init__(self):
+        pass
+    def generate_enemy(self):
+        pass
+
+
+class MeleeEnemayFactory(EnemyFactory):
+    def __init__(self):
+        super().__init__()
+
+    def generate_enemy(self):
+        pass
+
