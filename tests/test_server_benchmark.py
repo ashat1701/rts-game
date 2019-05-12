f11aae46affb32d36bea11b1e501e4cb473eb668
ashat1701 <ashat1701@github.com>
2019-05-12 22:53:53 +0300

    less data for stress tests

--- tests/test_server_benchmark.py
+++ tests/test_server_benchmark.py
@@ -33,9 +33,9 @@ def test_get_visible_enteties_benchmark(benchmark):
     logic.spawn_system.create_player(0)
     benchmark(logic.geometry_system.get_visible_tiles, 0)
 
-def test_animation_system_stress(benchmark):
-    logic = Logic()
-    spawn_enemies(100, logic)
-    benchmark(update_animation_and_move, logic)
-    despawn_enemies(100)
+#def test_animation_system_stress(benchmark):
+#    logic = Logic()
+#    spawn_enemies(100, logic)
+#    benchmark(update_animation_and_move, logic)
+#    despawn_enemies(100)
 
