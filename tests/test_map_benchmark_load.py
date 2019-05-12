798606bfd782257a67a16cef12a14083ab81aa8e
Alex Gryzlov <gryzlov.ad@yandex.ru>
2019-05-12 21:35:05 +0300

    add map creation load and stress tests

---
+++ tests/test_map_benchmark_load.py
@@ -0,0 +1,10 @@
+from src.Server.Map import Map
+
+
+def create_map(width, height, rooms, min_room_lenght, max_room_length, random_connections):
+    Map(width=width, height=height, max_rooms=rooms, min_room_len=min_room_lenght,
+        max_room_len=max_room_length, random_connections=random_connections)
+
+
+def test_map_benchmark_load(benchmark):
+    benchmark(create_map, 100, 100, 7, 7, 10, 4)
